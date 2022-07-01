c2a6670d29513080cce77cb18a546de8c0333b26
nomuyoshi <nomuyoshi0111@gmail.com>
2022-07-01 13:14:14 +0900

    component 分割

---
+++ src/component/Header.js
@@ -0,0 +1,15 @@
+
+function Header(props) {
+  return (
+    <section className="section has-background-primary has-text-light">
+      <h1 className='title is-4 has-text-light'>フリーランスエンジニア手取りシミュレーター</h1>
+      <p>
+      フリーランスエンジニアに特化した手取りシミュレーター。<br />
+      少ない入力項目で精度の高いシミュレーション可能です。
+      </p>
+    </section>
+  );
+}
+
+export default Header;
+
